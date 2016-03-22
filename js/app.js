fc76edfff81700a4cd830aae9eff94c10363fece
bgveenstra <b.g.veenstra@gmail.com>
2016-03-22 13:54:02 -0700

    handlebars version

--- js/app.js
+++ js/app.js
@@ -2,69 +2,307 @@ console.log('Sanity Check: JS is working!');
 
 $(document).ready(function(){
 
-  // code in here
-  // log name of course
-  console.log(course.name);
+  // select a template script, and pull out the handlebars and html inside it
+  var source = $('#student-li-template').html();
+  console.log(source);
 
-  // log whole list of students
-  console.log(course.students);
+  // compile the handlebars template
+  var template = Handlebars.compile(source);
 
-  // log all of first student's information
-  console.log(course.students[0]);
+  // use the template function from handlebars to create an HTML string
+  // the template function takes in an object where:
+    // each key is a variable the html template expects
+    // each key's value is the data we want that variable to have
+  var studentHtml27 = template({ students: data27.students });
 
-  // log first student's id
-  console.log(course.students[0].id);
+  var studentHtml28 = template({ students: data28.students });
 
-  // log first student's full name
-  console.log(course.students[0].firstName + 
-  	' ' + course.students[0].lastName);
-
-  // log number of students in class
-  console.log(course.students.length);
-
-  // log a list of GitHub usernames
-   course.students.forEach(function logGitHubName(student){
-  	console.log(student.gitHub);
-  });
-
-    // build a GitHub link from a single student's information
-	function buildGitHubLink(singleStudent){
-		return '<li><a href="' + singleStudent.gitHub + 
-			'">' + singleStudent.firstName + ' ' + 
-			singleStudent.lastName + '</a></li>';
-	}
-
-	// add github links to the page for all students
-	function renderGitHubLinks(allStudents){
-		allStudents.forEach(function addStudentToPage(student){
-			var gitHubLink = buildGitHubLink(student);
-			$studentList.append(gitHubLink);
-		});
-
-		// one line bonus version:
-		// $studentList.append(allStudents.map(buildGitHubLink).join(''));
-	}
-
-
-  var $studentList = $('.student-list');
-  renderGitHubLinks(course['students']);
+  // append html to the view
+  $("#students-list").append(studentHtml27);
+  $("#students-list").append(studentHtml28);
 
 });
 
-var course = {
-	name: "Web Development Immersive",
-	students: [
-		{
-			firstName: "Adam",
-			lastName: "nnnnnnnnnn",
-			gitHub: "annnnnnńn"
-		},
-		{
-			firstName: "b",
-			lastName: "bb",
-			gitHub: "bbbbbb"
-		}
-	]
-}
+var data27 = {
+  school: "General Assembly",
+  cohort: 27,
+  room: 1,
+  github_schedule: "https://github.com/sf-wdi-27-28/schedule-27",
+  students:[
+    {
+      first_name: 'Rene', 
+      last_name: 'Arellano',
+      github_username: 'relative-rene'
+    },
+    {
+      first_name: 'Keith',
+      last_name: 'Francis',
+      github_username: 'kjfrancis'
+    },
+    {
+      first_name: 'Joanne',
+      last_name: 'Abad',
+      github_username: 'Jabad28'
+    },
+     {
+      first_name: 'Marcelo',
+      last_name: 'leon',
+      github_username: 'Mel195'
+    },
+    {
+      first_name: 'Kirby',
+      last_name: 'Tate III',
+      github_username: 'shindomino' //When's Marvel?
+    },
+    {
+      first_name: 'Ayush',
+      last_name: 'Jha',
+      github_username: 'ayush'
+    },  
+    {
+      first_name: 'Jonathan',
+      last_name: 'Ortiz',
+      github_username: 'jortiz31'
+    },
+    {
+      first_name: 'Herman',
+      last_name: 'Chan',
+      github_username: 'hermchan'
+    },
+    {
+      first_name: "Phill",
+      last_name: "Cheng",
+      github_username: "PJC-1"
+    },
+    {
+      first_name: "Justin",
+      last_name: "Pettit",
+      github_username: "ahza64",
+      role: "Master of code destruction"
+    },
+    {
+      first_name: 'Paul',
+      last_name: 'Cleary',
+      git_username: 'Two-Watch-Boy'
+    },
+    {
+      first_name: "Jacquelyn",
+      last_name: "Fambrough",
+      github_username: "jacquelynfambrough"
+    },
+    {
+      first_name: "Bryan",
+      last_name: "Smith",
+      github_username: "smicl11"
+    },
+    {
+      first_name: 'Anna',
+      last_name: 'Sobolewska',
+      github_username: 'sobolewskaanna'
+    },
+    {
+      first_name: "Ada",
+      last_name: "Reid",
+      github_username: "adamrobertreid"
+    },
+    {
+      first_name: "Andrea",
+      last_name: "Ceballos",
+      githubewre_username: "andreaainara10"
+    },
+    {
+      first_name: "Ilter",
+      last_name: "Ates",
+      git_username: 'ilterates',
+      personality: "sassy"
+    },
+    {
+      first_name: "Sarah",
+      last_name: "Bannister",
+      github_username: "Srhbnnstr"
+    },
+    {
+      first_name: 'Nathan',
+      last_name: 'Lee',
+      github_username: 'timesize'
+    },
+    {
+      first_name: 'John',
+      last_name: 'Ruhsam',
+      github_username: 'ruhsam'
+    },
+    {
+      first_name: "Eric",
+      last_name: "Han",
+      github_username: "scoobaroo"
+    },
+    {
+      first_name: 'Arthur',
+      last_name: 'Tonelli',
+      github_username: 'altonelli'
+   }
+  ],
+  instructors: [
+    {
+      last_name: "Fauver",
+      first_name: "Cory",
+      github_username: "cofauver"
+    },
+    {
+      last_name: "Gaff",
+      first_name: "Travis",
+      github_username: "tgaff",
+      car: { 
+        make: 'kia',
+        model: 'cilantro',
+        year: 2019
+      }
+    },
+    {
+      last_name: "Veenstra",
+      first_name: "Brianna",
+      github_username: "bgveenstra"
+    },
+    {
+      role: "DiR",
+      first_name: "Wilbot",
+      last_name: "Cauthen",
+      github_username: "willcauthen"
+    }
+  ]
+};
 
-
+var data28 = {
+  school: "General Assembly",
+  name: "Web Development Immersive",
+  cohort: 28,
+  room: 2,
+  github_schedule: "https://github.com/sf-wdi-27-28/schedule-28",
+  instructors: [
+    "Juliana Lopker",
+    "Nathan Allen",
+    "Justin Castilla"
+  ],
+  students: [
+    {
+      id: 0,
+      last_name: "Vilhauer",
+      first_name: "Matthew",
+      github_username: "matthewvilhauer"
+    },
+    {
+      id: 1,
+      last_name: "Beck",
+      first_name: "Adam",
+      github_username: "mradambeck"
+    },
+    {
+      id: 2,
+      last_name: "Brown",
+      first_name: "Ameer",
+      github_username: "ameer-brown"
+    },
+    {
+      id: 3,
+      last_name: "Kliger",
+      first_name: "Bob",
+      github_username: "bkliger"
+    },
+    {
+      id: 4,
+      last_name: "Moses",
+      first_name: "Emily",
+      github_username:"eamoses"
+    },
+    {
+      id: 5,
+      last_name: "Zuniga",
+      first_name: "Franklin",
+      github_username: "Franklinz23"
+    },
+    {
+      id: 6,
+      last_name: "Lam",
+      first_name: "Christine",
+      github_username: "lamchristine"
+    },
+    {
+      id: 7,
+      last_name: "Tam",
+      first_name: "Justin",
+      github_username: "jtam11"
+    },
+    {
+      id: 8,
+      last_name: "Quach",
+      first_name:  "Lotus",
+      github_username: "lsquach"
+    },
+    {
+      id: 9,
+      last_name: "Carlson",
+      first_name: "Melissa",
+      github_username: "melicarls"
+    },
+    {
+      id: 10,
+      last_name: "Silverberg",
+      first_name: "Michael",
+      github_username: "mbd-s"
+    },
+    {
+      id: 11,
+      last_name: "Sharif",
+      first_name: "Morgan",
+      github_username: "morgansharif"
+    },
+    {
+      id: 12,
+      last_name: "Huang",
+      first_name: "Samuel",
+      github_username: "samuel1008"
+    },
+    {
+      id: 13,
+      last_name: "Madrid",
+      first_name: "Scott",
+      github_username:"samadrid92"
+    },
+    {
+      id: 14,
+      last_name: "Van Ho",
+      first_name: "Sean",
+      github_username: "seanvsville"
+    },
+    {
+      id: 15,
+      last_name: "Tekanyo",
+      first_name: "Jim",
+      github_username: "jimhantrix"
+    },
+    {
+      id: 16,
+      last_name: "Vaisler",
+      first_name: "Hila",
+      github_username: "hilava"
+    },
+    {
+      id: 17,
+      last_name: "Schwab",
+      first_name: "Anna",
+      github_username: "cloverharvest"
+    },
+    {
+      id: 18,
+      last_name: "Kitagawa",
+      first_name: "Chiyuki",
+      github_username: "chyktgw"
+    },
+    {
+      id: 19,
+      last_name: "Villagran",
+      first_name: "Adriana",
+      github_username: "AdrianaVillagran"
+    }
+  ]
+};
